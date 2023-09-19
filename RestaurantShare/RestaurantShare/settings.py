046f70af65e960f547260b8574cc68ab70e192d2
MegaZizon <wnsdh8846@naver.com>
2023-09-20 03:03:44 +0900

    project done

--- RestaurantShare/RestaurantShare/settings.py
+++ RestaurantShare/RestaurantShare/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,11 +77,15 @@ WSGI_APPLICATION = "RestaurantShare.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+DATABASES = { 
+	'default': { 
+    	'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'test_db', 
+        'USER': 'root', 
+        'PASSWORD': '1234', 
+        'HOST': 'localhost', 
+        'PORT': '3306', 
+     } 
 }
 
 
